fe3c97fa5dacf74eeea08aa49a3481444cebda13
Laiz D <megami.ay@gmail.com>
2024-04-09 11:02:24 -0300

    controller e notfoundcontroller exp

--- controllers/notFoundContoller.php
+++ controllers/notFoundContoller.php
@@ -1,10 +1,10 @@
 <?php 
 class notFoundContoller extends Controller {
 
-    public function index(){
-        $data = array();
+    public function index(){                             // a função index() é usada como o ponto de entrada padrão para um controlador. isso significa que quando nenhuma função específica é fornecida na URL, a função index() é chamada por padrão.
+        $data = array();                                 // estabelece os dados a serem passados para a função loadView()
 
-        $this->loadView('404', $data);
+        $this->loadView('404', $data);                   // o objeto ($this), passa (->) dois parâmetros para o loadView: os parâmetros a serem enviados para $viewName e para $viewData, respectivamente = loadView('nomedaview', $data) 
     }
 
 }
--- core/Controller.php
+++ core/Controller.php
@@ -10,6 +10,34 @@ class Controller {                      // aqui ira ficar as funções principai
     public function loadView($viewName, $viewData = array()){       // essa função é utilizada dentro dos controllers... a função é responsável por carregar na view e passar dados para ela
         extract($viewData);                                         // extrai os dados da view para que possam ser acessados
         include 'views/'.$viewName.'.php';                          // inclue os dados da view especificada, é utilizado da seguinte forma... por exemplo no controller notFoundContoller.php:
-    }                                                               // $this->loadView('404', $data); ... $this-> usado dentro da função para acessar propriedades e funções da própria classe (Controller)... o objeto ($this), passa (->) dois parâmetros para o loadView: dentro de () temos os parâmetros a serem enviados para $viewData e para $viewName = loadView('nomedaview', $data)
+        // EXEMPLO: $this->loadView('404', $data); ... $this-> usado dentro da função para acessar propriedades e funções da própria classe (Controller)... o objeto ($this), passa (->) dois parâmetros para o loadView: dentro de () temos os parâmetros a serem enviados para $viewData e para $viewName = loadView('nomedaview', $data)
+    }                                                               
+
+    public function loadTemplate($viewName, $viewData = array()){
+        include 'view/template.php';
+    }
+
+    public function loadViewInTemplate($viewName, $viewData) {      // função especifica para página de template
+        extract($viewData);
+        include 'views/'.$viewName.'.php';
+    }	
 
 }
+
+// DIFERENÇA ENTRE loadView E loadViewInTemplate:
+// ambos os métodos têm a mesma funcionalidade principal, que é carregar uma view em um template e extrair os dados para que possam ser usados na view.
+
+//loadView($viewName, $viewData = array()):
+
+// esta função tem um parâmetro opcional ($viewData), que é iniciado como um array vazio caso não seja fornecido o $viewData.
+// ou seja, você pode chamar loadView() sem dar informações ao segundo parâmetro ($viewData), e ele ainda funcionará. exemplo: $this->loadView('nomedaview');
+// e se você passar o segundo parâmetro $viewData, ele será extraído e usado na view, como é o caso em extract($viewData);.
+
+// já em:
+
+// loadViewInTemplate($viewName, $viewData):
+
+// este método não tem um valor padrão para $viewData, o que significa que você deve sempre fornecer um array de dados quando chamar esta função. exemplo: $objeto->loadViewInTemplate('nomedaview', $data);
+// não há verificação interna para garantir que $viewData seja realmente um array. se chamar loadViewInTemplate() sem um segundo argumento ou com um argumento que não seja um array, isso pode resultar em um erro de execução.
+
+// resumo, a diferença é que loadView() permite que você chame a função sem fornecer explicitamente os dados da view, enquanto loadViewInTemplate() requer que você sempre forneça esses dados.
